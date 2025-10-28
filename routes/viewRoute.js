956c1872254cc8e9dba39f6260661462980f04b3
celinaaee <ceel24ab@student.cbs.dk>
2025-10-28 10:12:28 +0100

    n

--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/view.html'));
