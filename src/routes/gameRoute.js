bd83daa134c9f82e320b2b424152472b4348a5e9
celinaaee <ceel24ab@student.cbs.dk>
2025-12-11 19:12:57 +0100

    rengøring

--- src/routes/gameRoute.js
+++ src/routes/gameRoute.js
@@ -1,20 +1,3 @@
-//original
-//routes/gameRoute.js
-/*
-const express = require("express");
-const { spinWheel, checkSpin } = require("../controllers/gameController");
-const requireAuth = require("../middleware/auth");
-
-const router = express.Router();
-
-//tjek først om brugeren må spinne i dag
-router.get("/check", requireAuth, checkSpin);
-//selve spin routen, som sender SMS osv.
-router.post("/spin", requireAuth, spinWheel);
-
-module.exports = router;
-*/
-
 // src/routes/gameRoute.js
 const express = require("express");
 const { spinWheel, checkSpin, getPrizes } = require("../controllers/gameController");
